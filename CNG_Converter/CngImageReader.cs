dac27fb2c5120297d6f3ab8844448a30bac73584
Aaron Havens <aaron@toomanymiles.net>
2018-07-25 20:55:27 -0500

    Extracted the CNG conversion into its own class.

---
+++ CNG_Converter/CngImageReader.cs
@@ -0,0 +1,56 @@
+﻿using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+
+namespace CNG_Converter
+{
+    internal class CngImageReader : BinaryReader
+    {
+        private const int XorValue = 239;
+
+        public CngImageReader(Stream input) : base(input)
+        {
+        }
+
+        public CngImageReader(Stream input, Encoding encoding) : base(input, encoding)
+        {
+        }
+
+        public CngImageReader(Stream input, Encoding encoding, bool leaveOpen) : base(input, encoding, leaveOpen)
+        {
+        }
+
+        public override byte[] ReadBytes(int count)
+        {
+            var tmpBuffer = new byte[count];
+            var buffer = new byte[count];
+            var length = 0;
+
+            do
+            {
+                var num = this.BaseStream.Read(tmpBuffer, length, count);
+                if (num != 0)
+                {
+                    length += num;
+                    count -= num;
+                    for (var i = 0; i < num; i++)
+                    {
+                        buffer[i] = (byte) (tmpBuffer[i] ^ XorValue);
+                    }
+                }
+                else
+                {
+                    break;
+                }
+            } while (count > 0);
+
+            return buffer;
+        }
+
+        public override int Read()
+        {
+            return this.ReadBytes(1).First();
+        }
+    }
+}
---
+++ CNG_Converter/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.IO;
+
+namespace CNG_Converter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputFile = @"E:\NGM_1996_05_001_4.cng";
+            var outputFile = @"E:\outputfile.jpg";
+            
+            var fileInfo = new FileInfo(inputFile);
+            var fileLength = fileInfo.Length;
+
+            using (var fs = fileInfo.OpenRead())
+            {
+                using(var os = new FileStream(outputFile, FileMode.Create, FileAccess.Write))
+                {
+                    var br = new CngImageReader(fs);//new BinaryReader(fs);
+
+                    var buffer = br.ReadBytes((int) fileLength);
+                    var bw = new BinaryWriter(os);
+
+                    for (var i = 0; i < fileLength; i++)
+                    {
+                        bw.Write(buffer[i]);
+                    }
+                }
+            }
+        }
+    }
+}
