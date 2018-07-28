eac5728e08c6536ecb6625f5bf9b13a2a2cf83f9
Aaron Havens <aaron@toomanymiles.net>
2018-07-27 23:29:05 -0500

    Updated unit test data to remove warnings about duplicate data.

--- CngImageReaderTests/UnitTests.cs
+++ CngImageReaderTests/UnitTests.cs
@@ -12,7 +12,7 @@ namespace CngImageReaderTests
         [Theory]
         [InlineData(1)]
         [InlineData('A')]
-        [InlineData(0x01)]
+        [InlineData(0x09)]
         public void CngImageReader_ReadByte_Modifies_Stream_Data_Test(byte data)
         {
             // Arrange
@@ -28,7 +28,6 @@ namespace CngImageReaderTests
 
         [Theory]
         [InlineData(new byte[] {1, 2, 3, 4, 5, 6, 7, 8})]
-        [InlineData(new byte[] {0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08})]
         public void CngImageReader_ReadBytes_Returns_Number_Bytes_Requested_If_Stream_Contains_Length_Test(byte[] data)
         {
             // Arrange
@@ -45,7 +44,6 @@ namespace CngImageReaderTests
 
         [Theory]
         [InlineData(new byte[] {1, 2, 3, 4, 5, 6, 7, 8})]
-        [InlineData(new byte[] {0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08})]
         public void CngImageReader_ReadBytes_Returns_Specified_Byte_Length_Data_Test(byte[] data)
         {
             // Arrange
